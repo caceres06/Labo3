07f48703f8fbf6dcc51c17f5615c6c91703db244
caceres06 <37302560+caceres06@users.noreply.github.com>
2019-04-08 23:32:07 -0600

    te me cuidas alv

--- src/labo3/Cine.java
+++ src/labo3/Cine.java
@@ -14,12 +14,11 @@ import java.util.Scanner;
  */
 public class Cine {
     
-    private String nombre;
-    private String lema;
+    private String nombre= "Cinepolis";
+    private String lema="Cinepolis, la capital del cine";
     ArrayList <Funcion> funcion;
 
-    public Cine(String nombre) {
-        this.nombre = nombre;
+    public Cine() {
     }
 
     public Cine(String nombre, String lema) {
@@ -52,11 +51,48 @@ public class Cine {
      int numeFuncion= scan.nextInt();
      scan.nextLine();
      ArrayList<Funcion> f= new ArrayList<>();
+     Funcion fun = new Funcion(diaFuncion,numeFuncion);
      for(int i=0;i<f.size();i++){
-         if(!diaFuncion || numeFuncion !=i) {
-         } else {
-             f.add(new Funcion(diaFuncion,numeFuncion));
+         Funcion aux =f.get(i);
+         if(fun==aux){
+             throw new IllegalStateException("Esta funcion ya existe, por favor ingrese otros datos");
+         } else{
+             f.add(fun);
+         }
          }
      }
+public static void EditarFuncion(){
+Scanner scan = new Scanner(System.in);
+     System.out.println("Ingrese el día de la función");
+     String diaFuncion = scan.next();
+     System.out.println("Ingrese el número de la función");
+     int numeFuncion= scan.nextInt();
+     scan.nextLine();
+     ArrayList<Funcion> f= new ArrayList<>();
+     Funcion fun = new Funcion(diaFuncion,numeFuncion);
+     for(int i=0;i<f.size();i++){
+         Funcion aux =f.get(i);
+         if(fun==aux){
+             System.out.println("Ingrese el nuevo día de la función");
+             fun.setDia(scan.next());
+             System.out.println("Ingrese el nuevo número de función");
+             fun.setNumfucion(scan.nextInt());
+             scan.nextLine();
+         } else{
+             throw new IllegalStateException("Esta funcion no existe, por favor ingrese otra función para modificar");
+         }
+         }
+     }
+public static void ImprimirCartelera(){
+    ArrayList<Funcion> f= new ArrayList<>();
+    for(int i=0;i<f.size();i++){
+        System.out.println("-----CARTELERA-----");
+        System.out.println(f.get(i));
     }
+}
+public static void InfoCine(){
+    Cine cine = new Cine();
+    System.out.println("Nombre del cine:"+" "+cine.getNombre());
+    System.out.println(cine.getLema());
+}
 }
--- src/labo3/Labo3.java
+++ src/labo3/Labo3.java
@@ -17,7 +17,33 @@ public class Labo3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        boolean flag = true;
         Scanner key = new Scanner(System.in);
+        System.out.println("Bienvenido");
+        System.out.println("¿Qué desea hacer?");
+        System.out.println("1.Ver información del cine");
+        System.out.println("3.Editar función");
+        System.out.println("4.Imprimir cartelera");
+        System.out.println("5. Salir");
+        int opc= key.nextInt();
+        Menu(opc);
     }
-    
+    public static void Menu(int a){
+        switch(a){
+            case 1:
+                Cine.InfoCine();
+                break;
+            case 2:
+                Cine.AgregarFuncion();
+                break;
+            case 3:
+                Cine.EditarFuncion();
+                break;
+            case 4:
+                Cine.ImprimirCartelera();
+                break;
+            case 5:
+                break;
+        }
+    }
 }
